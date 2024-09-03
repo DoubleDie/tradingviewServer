c0f3cf15fc5aa6c8f29b6f8aadb749785ae74646
DoubleDie <robcheck0102@gmail.com>
2024-09-03 22:39:10 +1000

    fixed server

--- server.js
+++ server.js
@@ -16,22 +16,7 @@ const server = http.createServer( (req, res) => {
             console.log(`Raw Body: ${body}`)
             fs.writeFile('last_signal.txt', body, (err) => {
                 console.log("Error: ", err)
-            })
-            try {
-                parsed = JSON.parse(body)
-                console.log("Parsed: ", parsed)
-                res.writeHead(200, headers)
-                res.end("Response sent")
-            } catch (err) {
-                console.log(`Error: ${err}`)
-                console.log(`Request causing error: ${body}`)
-                let errorHeaders = {
-                    "Content-Type": "application/json",
-                    "status": "error"
-                }
-                res.writeHead(400, errorHeaders)
-                res.end("Error: ", err)
-            }
+            })         
         })
     }
 });
